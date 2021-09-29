042d887fe42f37cb26de9ed1602b096e58d7ef2a
Junior Paz Formoso <junior.presagios@gmail.com>
2021-09-29 17:56:46 +0300

    Register bundle resource i18n

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,12 +1,17 @@
 sap.ui.define(
-  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
-  function(UIComponent, JSONModel) {
+  [
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function(UIComponent, ResourceModel, JSONModel) {
     "use strict";
     return UIComponent.extend("sap.demo.todo.Component", {
       metadata: {
         rootView: "sap.demo.todo.view.App",
         config: {
           serviceUrl: "/service/todoitems.json",
+          i18nBundle: "sap.demo.todo.i18n.i18n",
         },
         routing: {
           config: {
@@ -34,8 +39,17 @@ sap.ui.define(
         },
       },
       init: function() {
+        var mConfig = this.getMetadata().getConfig();
+
         // call the base component's init function
         UIComponent.prototype.init.apply(this, arguments);
+        // set the internationalization model
+        this.setModel(
+          new ResourceModel({
+            bundleName: mConfig.i18nBundle,
+          }),
+          "i18n"
+        );
 
         // create the views based on the url/hash
         this.getRouter().initialize();
