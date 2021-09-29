12d1f031f1186f76c1baac42121ca180256f0120
Junior Paz Formoso <junior.presagios@gmail.com>
2021-09-29 16:49:40 +0300

    set todo model on root Component

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,47 +1,57 @@
-sap.ui.define(["sap/ui/core/UIComponent"], function(UIComponent) {
-  "use strict";
-  return UIComponent.extend("sap.demo.todo.Component", {
-    metadata: {
-      rootView: "sap.demo.todo.view.App",
-      routing: {
+sap.ui.define(
+  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
+  function(UIComponent, JSONModel) {
+    "use strict";
+    return UIComponent.extend("sap.demo.todo.Component", {
+      metadata: {
+        rootView: "sap.demo.todo.view.App",
         config: {
-          routerClass: "sap.m.routing.Router",
-          viewType: "XML",
-          viewPath: "sap.demo.todo.view",
-          controlId: "app",
-          controlAggregation: "pages",
-          transition: "slide",
+          serviceUrl: "/service/todoitems.json",
         },
-        routes: [
-          {
-            pattern: "",
-            name: "master",
-            target: "master",
+        routing: {
+          config: {
+            routerClass: "sap.m.routing.Router",
+            viewType: "XML",
+            viewPath: "sap.demo.todo.view",
+            controlId: "app",
+            controlAggregation: "pages",
+            transition: "slide",
           },
-        ],
+          routes: [
+            {
+              pattern: "",
+              name: "master",
+              target: "master",
+            },
+          ],
 
-        targets: {
-          master: {
-            viewName: "Master",
-            viewLevel: 1,
+          targets: {
+            master: {
+              viewName: "Master",
+              viewLevel: 1,
+            },
           },
         },
       },
-    },
-    init: function() {
-      // call the base component's init function
-      UIComponent.prototype.init.apply(this, arguments);
+      init: function() {
+        // call the base component's init function
+        UIComponent.prototype.init.apply(this, arguments);
 
-      // create the views based on the url/hash
-      this.getRouter().initialize();
-    },
-    createContent: function() {
-      var oRootView = UIComponent.prototype.createContent.apply(
-        this,
-        arguments
-      );
+        // create the views based on the url/hash
+        this.getRouter().initialize();
+      },
+      createContent: function() {
+        var oRootView = UIComponent.prototype.createContent.apply(
+          this,
+          arguments
+        );
 
-      return oRootView;
-    },
-  });
-});
+        var oModel = new JSONModel(this.getMetadata().getConfig().serviceUrl);
+
+        this.setModel(oModel);
+
+        return oRootView;
+      },
+    });
+  }
+);
