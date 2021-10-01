760bdfebeac14541bf6bb21ba4cff014ba919280
Junior Paz Formoso <junior.presagios@gmail.com>
2021-10-01 10:58:29 +0300

    Added Should toggle the completed items in the model

--- webapp/test/unit/controller/App.controller.js
+++ webapp/test/unit/controller/App.controller.js
@@ -47,5 +47,44 @@ sap.ui.define(
         "There is one new item."
       );
     });
+
+    QUnit.test("Should toggle the completed items in the model", function(
+      assert
+    ) {
+      // Arrange
+      var oModelData = {
+        todos: [
+          {
+            title: "Start this app",
+            completed: false,
+          },
+        ],
+        itemsLeftCount: 1,
+      };
+      this.oJSONModelStub.setData(oModelData);
+
+      // initial assumption
+      assert.strictEqual(
+        this.oJSONModelStub.getObject("/todos").length,
+        1,
+        "There is one item."
+      );
+      assert.strictEqual(
+        this.oJSONModelStub.getProperty("/itemsLeftCount"),
+        1,
+        "There is one item left."
+      );
+
+      // Act
+      this.oJSONModelStub.setProperty("/todos/0/completed", true);
+      this.oAppController.updateItemsLeftCount();
+
+      // Assumption
+      assert.strictEqual(
+        this.oJSONModelStub.getProperty("/itemsLeftCount"),
+        0,
+        "There is no item left."
+      );
+    });
   }
 );
