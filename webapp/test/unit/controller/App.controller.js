6f1880582417a9b8c6656aff1cdcfac3a1a406aa
Junior Paz Formoso <junior.presagios@gmail.com>
2021-10-01 09:59:49 +0300

    Added test Should add a todo element to the model

--- webapp/test/unit/controller/App.controller.js
+++ webapp/test/unit/controller/App.controller.js
@@ -26,5 +26,26 @@ sap.ui.define(
         this.oViewStub.destroy();
       },
     });
+
+    QUnit.test("Should add a todo element to the model", function(assert) {
+      // Arrange
+      // initial assumption: to-do list is empty
+      assert.strictEqual(
+        this.oJSONModelStub.getObject("/todos").length,
+        0,
+        "There must be no todos defined."
+      );
+
+      // Act
+      this.oJSONModelStub.setProperty("/newTodo", "new todo item");
+      this.oAppController.addTodo();
+
+      // Assumption
+      assert.strictEqual(
+        this.oJSONModelStub.getObject("/todos").length,
+        1,
+        "There is one new item."
+      );
+    });
   }
 );
