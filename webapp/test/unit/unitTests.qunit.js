88f20a96423c9ff207e5db0d7d18cb7918b7e069
Junior Paz Formoso <junior.presagios@gmail.com>
2021-10-01 17:50:26 +0300

    Refactoring (Change double quote to single ones)

--- webapp/test/unit/unitTests.qunit.js
+++ webapp/test/unit/unitTests.qunit.js
@@ -3,9 +3,9 @@
 QUnit.config.autostart = false;
 
 sap.ui.getCore().attachInit(function() {
-  "use strict";
+  'use strict';
 
-  sap.ui.require(["sap/demo/todo/test/unit/AllTests"], function() {
+  sap.ui.require(['sap/demo/todo/test/unit/AllTests'], function() {
     QUnit.start();
   });
 });
