270cf553db94051993fb9f1a39e4fc39e4f4c388
Junior Paz Formoso <junior.presagios@gmail.com>
2021-10-01 09:28:49 +0300

    Added filter capabilities
    Filter by todo completed, active, all and added filter by contain:text

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -44,9 +44,11 @@ sap.ui.define(
 
       clearCompleted: function() {
         var oModel = this.getView().getModel();
-        var aTodos = oModel.getProperty("/todos").map(function(oTodo) {
-          return Object.assign({}, oTodo);
-        });
+        // var aTodos = oModel.getProperty("/todos").map(function(oTodo) {
+        //   return Object.assign({}, oTodo);
+        // });
+
+        var aTodos = oModel.getProperty("/todos");
 
         var i = aTodos.length;
         while (i--) {
@@ -71,6 +73,94 @@ sap.ui.define(
         }).length;
 
         oModel.setProperty("/itemsLeftCount", iItemsLeft);
+      },
+
+      /**
+		 * Trigger search for specific items. The removal of items is disable as long as the search is used.
+		 * @param {sap.ui.base.Event} oEvent Input changed event
+		 */
+      onSearch: function(oEvent) {
+        var oModel = this.getView().getModel();
+
+        // First reset current filters
+        this.aSearchFilters = [];
+
+        // add filter for search
+        this.sSearchQuery = oEvent.getSource().getValue();
+        if (this.sSearchQuery && this.sSearchQuery.length > 0) {
+          oModel.setProperty("/itemsRemovable", false);
+          var filter = new Filter(
+            "title",
+            FilterOperator.Contains,
+            this.sSearchQuery
+          );
+          this.aSearchFilters.push(filter);
+        } else {
+          oModel.setProperty("/itemsRemovable", true);
+        }
+
+        this._applyListFilters();
+      },
+
+      onFilter: function(oEvent) {
+        // First reset current filters
+        this.aTabFilters = [];
+
+        // add filter for search
+        this.sFilterKey = oEvent.getParameter("item").getKey();
+
+        // eslint-disable-line default-case
+        switch (this.sFilterKey) {
+          case "active":
+            this.aTabFilters.push(
+              new Filter("completed", FilterOperator.EQ, false)
+            );
+            break;
+          case "completed":
+            this.aTabFilters.push(
+              new Filter("completed", FilterOperator.EQ, true)
+            );
+            break;
+          case "all":
+          default:
+          // Don't use any filter
+        }
+
+        this._applyListFilters();
+      },
+
+      _applyListFilters: function() {
+        var oList = this.byId("todoList");
+        var oBinding = oList.getBinding("items");
+
+        oBinding.filter(this.aSearchFilters.concat(this.aTabFilters), "todos");
+
+        var sI18nKey;
+        if (this.sFilterKey && this.sFilterKey !== "all") {
+          if (this.sFilterKey === "active") {
+            sI18nKey = "ACTIVE_ITEMS";
+          } else {
+            // completed items: sFilterKey = "completed"
+            sI18nKey = "COMPLETED_ITEMS";
+          }
+          if (this.sSearchQuery) {
+            sI18nKey += "_CONTAINING";
+          }
+        } else if (this.sSearchQuery) {
+          sI18nKey = "ITEMS_CONTAINING";
+        }
+
+        var sFilterText;
+        if (sI18nKey) {
+          var oResourceBundle = this.getView()
+            .getModel("i18n")
+            .getResourceBundle();
+          sFilterText = oResourceBundle.getText(sI18nKey, [this.sSearchQuery]);
+        }
+
+        this.getView()
+          .getModel("view")
+          .setProperty("/filterText", sFilterText);
       },
     });
   }
